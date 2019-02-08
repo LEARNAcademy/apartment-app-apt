7b2b53a3761c1af658f10af872b283d6f8a11525
learn <learn@learn12.localdomain>
2019-02-08 13:33:38 -0800

    added apartment list

--- app/javascript/components/pages/Apartments.js
+++ app/javascript/components/pages/Apartments.js
@@ -2,12 +2,46 @@ import React from "react"
 import PropTypes from "prop-types"
 
 class Apartments extends React.Component {
-  
+  constructor(props){
+    super(props)
+    this.state = {
+      apartments: []
+    }
+  }
+
+
   render () {
     return (
-      <h1>Apartment List</h1>
+      <div>
+        <h1>Apartment List</h1>
+          <table>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Street</th>
+                <th>Street</th>
+                <th>City</th>
+                <th>State</th>
+                <th>Country</th>
+                <th>Postal Code</th>
+              </tr>
+                {this.state.apartments.map((a, i) => {
+                  return(
+                      <tr key={i}>
+                        <td>{a.name}</td>
+                        <td>{a.street_1}</td>
+                        <td>{a.street_2}</td>
+                        <td>{a.city}</td>
+                        <td>{a.state}</td>
+                        <td>{a.country}</td>
+                        <td>{a.postal_code}</td>
+                      </tr>
+                  )
+                })}
+            </tbody>
+          </table>
+      </div>
     );
   }
 }
-
 export default Apartments
